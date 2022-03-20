38f627825986f9d8c8bc8f17772a1719264243c5
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-21 00:48:16 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -6,15 +6,18 @@ if [ -f "index.json" ]
     limit=3
     failures=`cat index.json | jq ".stats.failures"`
     jq -c '.results[].suites[]' index.json | while read -r i && [[ "$limit" != 0 ]]; do
-      ((limit--))
-      title=$(echo "$i" | jq '.tests[0].title')
-      file=$(echo "$i" | jq '.fullFile')
-      message=$(echo "$i" | jq '.tests[0].err.message')
-      run_id=$(echo "$i" | jq '.uuid')
-      report=$(echo ":test_tube:*TEST*: $title \n:open_file_folder:*FILE*: <https://cypress-dashboard.staging.manabie.io:31600/instance/$run_id | $file> \n:speech_balloon:*MESSAGE*: $message \n")
-      full_report+="$report \n"
-      if [[ $limit == 0 ]]; then
-          full_report+="...showing 3 of ${failures} test fails"
+      isFail=$(echo "$i" | jq '.tests[0].fail')
+      if [[ $isFail ]]; then
+        ((limit--))
+        title=$(echo "$i" | jq '.tests[0].title')
+        file=$(echo "$i" | jq '.fullFile')
+        message=$(echo "$i" | jq '.tests[0].err.message')
+        run_id=$(echo "$i" | jq '.uuid')
+        report=$(echo ":test_tube:*TEST*: $title \n:open_file_folder:*FILE*: <https://cypress-dashboard.staging.manabie.io:31600/instance/$run_id | $file> \n:speech_balloon:*MESSAGE*: $message \n")
+        full_report+="$report \n"
+        if [[ $limit == 0 ]]; then
+            full_report+="...showing 3 of ${failures} test fails"
+        fi
       fi
       full_report=$(echo ${full_report//$'\n'/'%0A'} | sed 's/"//g')
       echo "::set-output name=fail_count::"${failures}""
