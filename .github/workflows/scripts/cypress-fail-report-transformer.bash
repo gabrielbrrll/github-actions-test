c7b2e5df59e31aff6813faf7bd3e4990463c2a69
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-20 19:36:00 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -16,16 +16,17 @@ if [ -f "index.json" ]
           full_report+="...showing 3 of ${failures} test fails"
       fi
       echo "::set-output name=fail_count::"${failures}""
+      full_report=$(cat << EOF
+        $full_report
+      EOF
+      )
+
+      full_report=$(echo ${full_report//$'\n'/'%0A'} | sed 's/"//g')
+      echo "${{ full_report }}"
+      echo "::set-output name=fail_report::"${full_report}""
     done
   else
     echo "No failed"
 fi
 
-full_report=$(cat << EOF
-  "${{ full_report }}"
-EOF
-)
-  
-full_report=$(echo ${full_report//$'\n'/'%0A'} | sed 's/"//g')
-echo "${{ full_report }}"
-echo "::set-output name=fail_report::"${full_report}""
+
