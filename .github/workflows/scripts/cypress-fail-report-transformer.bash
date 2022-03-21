b58b0e80da0545557f569f07551fa7c7beba78e4
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-22 07:59:38 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -4,31 +4,32 @@ if [ -f "index.json" ]
   then
   full_report=""
   limit=3
-  raw_report=`cat index.json`
-  echo "${raw_report} -- ++RAW REPORT"
+  
   failures=`cat index.json | jq ".stats.failures"`
-  jq -c '.results[].suites[]' index.json | while read -r i && [[ "$limit" != 0 ]]; do
-    fail_count=$(echo "$i" | jq '.failures | length')
-    echo "$fail_count +++FAIL COUNT+++"
-    if [[ $fail_count -gt 0 ]]; then
-      ((limit--))
-      title=$(echo "$i" | jq '.tests[0].title')
-      file=$(echo "$i" | jq '.fullFile')
-      message=$(echo "$i" | jq '.tests[0].err.message')
-      run_id=$(echo "$i" | jq '.uuid')
-      if [[ $fail_count -gt 1 ]]; then
-          title+=" (showing 1 out of $fail_count tests failing"
-      fi
-      report=$(echo ":test_tube:*TEST*: $title \n:open_file_folder:*FILE*: <https://cypress-dashboard.staging.manabie.io:31600/instance/$run_id | $file> \n:speech_balloon:*MESSAGE*: $message \n")
-      full_report+="$report \n"
-      if [[ $limit == 0 ]]; then
-          full_report+="...showing 3 of ${failures} test suite fails"
-      fi
+  
+  jq -c '.results[].suites[]' index.json | while read -r i && [[ $limit != 0 ]]; do
+    title=$(echo "$i" | jq '.tests[0].title')
+    file=$(echo "$i" | jq '.fullFile')
+    message=$(echo "$i" | jq '.tests[0].err.message')
+    run_id=$(echo "$i" | jq '.uuid')
+    
+    if [[ $fail_count -gt 1 ]]; then
+        title+=" (showing 1 out of $fail_count tests failing"
     fi
-    echo "$full_report +++FULL REPORT - BEFORE REMOVING QUOTES"
+    
+    report=$(echo ":test_tube:*TEST*: $title \n:open_file_folder:*FILE*: <https://cypress-dashboard.staging.manabie.io:31600/instance/$run_id | $file> \n:speech_balloon:*MESSAGE*: $message \n")
+    
+    if [[ $fail_count -gt 0]]; then
+        full_report+="$report /n"
+        ((limit--))
+    fi
+    
+    if [ $limit == 0 ]; then
+        full_report+="...showing 3 of ${failures} test suite fails"
+    fi
+    
     full_report=$(echo ${full_report//$'\n'/'%0A'} | sed 's/"//g')
-    echo "$full_report +++FULL REPORT - AFTER REMOVING QUOTES"
-    echo "::set-output name=fail_count::"${failures}""
+    echo "::set-output name=fail_count::$failures"
     echo "::set-output name=fail_report::$full_report"
   done
   else
