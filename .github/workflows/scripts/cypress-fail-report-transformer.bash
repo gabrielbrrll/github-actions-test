f92d00f7d3f71d6e2cbdb2cd51d06a177b87b2b4
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-24 07:55:18 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -31,7 +31,16 @@ function generate_report(){
       echo ${result} | base64 --decode | jq -r ${1}
     }
     title=$(_jq '.tests[0].title')
-    echo $title
+    file=$(_jq '.fullFile')
+    message=$(_jq '.tests[0].err.message')
+    run_id=$(_jq '.tests[0].uuid')
+    report=$(echo ":test_tube:*TEST*: $title \n:open_file_folder:*FILE*: <https://cypress-dashboard.staging.manabie.io:31600/run/$cypress_run_id | $file> \n:speech_balloon:*MESSAGE*: $message \n")
+    full_report+="$report /n"
+    ((limit--))
+    full_report=$(echo ${full_report//$'\n'/'%0A'} | sed 's/"//g')
+    echo "::set-output name=fail_count::$total_fails"
+    echo "::set-output name=fail_report::$full_report"
+    echo $full_report
   done
 }
 
