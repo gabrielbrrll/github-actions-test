319df3c94b2c4c286cce9803cd963f7c600ddaa9
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-23 23:31:53 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -26,7 +26,8 @@ function generate_report(){
   local total_fails=`cat index.json | jq ".stats.failures"`
   local fail_results=`cat index.json | jq -c '.results[].suites[] | select(.failures | length > 0)'`
   local cypress_run_id=$(echo "${{ steps.run-integration.outputs.dashboardUrl }}" | sed 's:.*/::')
-  jq -c "$fail_results" index.json | while read -r i && [[ $limit != 0 ]]; do
+  echo $fail_results
+  jq -c $fail_results | while read -r i && [[ $limit != 0 ]]; do
     title=$(echo "$i" | jq '.tests[0].title')
     file=$(echo "$i" | jq '.fullFile')
     message=$(echo "$i" | jq '.tests[0].err.message')
@@ -35,7 +36,6 @@ function generate_report(){
     full_report+="$report /n"
     ((limit--))
     full_report=$(echo ${full_report//$'\n'/'%0A'} | sed 's/"//g')
-    echo $full_report
     echo "::set-output name=fail_count::$total_fails"
     echo "::set-output name=fail_report::$full_report"
     echo $full_report
