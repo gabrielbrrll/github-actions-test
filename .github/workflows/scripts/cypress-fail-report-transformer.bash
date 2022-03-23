6ebf9ae830cf09a348edc8521096255e7adca610
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-24 00:23:33 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -27,7 +27,7 @@ function generate_report(){
   local fail_results=`cat ${REPORT_FILE} | jq -c '[.results[].suites[] | select(.failures | length > 0)]'`
   local cypress_run_id=$(echo "${{ steps.run-integration.outputs.dashboardUrl }}" | sed 's:.*/::')
   echo $fail_results
-  jq -c ".[]" ${fail_results} | while read -r i && [[ $limit != 0 ]]; do
+  jq -r ".[]" ${fail_results} | while read -r i && [[ $limit != 0 ]]; do
     echo "$i ++++"
 #     title=$(echo "$i" | jq '.tests[0].title')
 #     file=$(echo "$i" | jq '.fullFile')
