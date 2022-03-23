08ef60158047ee98e66ea929a9e00d78770b90a2
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-23 23:41:22 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -24,9 +24,10 @@ function generate_report(){
   local limit=3
   local failures=`cat ${REPORT_FILE} | jq ".stats.failures"`
   local total_fails=`cat ${REPORT_FILE} | jq ".stats.failures"`
-  local fail_results=`cat ${REPORT_FILE} | jq -c '.results[].suites[] | select(.failures | length > 0)'`
+  local fail_results=`cat ${REPORT_FILE} | jq -c 'select(.results[].suites[].failures | length > 0)'`
   local cypress_run_id=$(echo "${{ steps.run-integration.outputs.dashboardUrl }}" | sed 's:.*/::')
-  jq -c ${fail_results} ${REPORT_FILE} | while read -r i && [[ $limit != 0 ]]; do
+  echo $fail_results
+  jq -c ${fail_results} | while read -r i && [[ $limit != 0 ]]; do
     title=$(echo "$i" | jq '.tests[0].title')
     file=$(echo "$i" | jq '.fullFile')
     message=$(echo "$i" | jq '.tests[0].err.message')
@@ -37,7 +38,6 @@ function generate_report(){
     full_report=$(echo ${full_report//$'\n'/'%0A'} | sed 's/"//g')
     echo "::set-output name=fail_count::$total_fails"
     echo "::set-output name=fail_report::$full_report"
-    echo $full_report
   done
 }
 
