39efc247d76b68850c339b64d2a626cd42662f95
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-24 08:17:29 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -36,9 +36,8 @@ function generate_report(){
     run_id=$(_jq '.uuid')
     report=$(echo ":test_tube:*TEST*: $title \n:open_file_folder:*FILE*: <https://cypress-dashboard.staging.manabie.io:31600/run/$cypress_run_id | $file> \n:speech_balloon:*MESSAGE*: $message \n\n")
     full_report+="$report"
+    full_report+="/nShowing 3 out of ${total_fails} test suite fails."
     if [[ $limit -eq 0 ]]; then
-      echo $limit
-      full_report+="/nShowing 3 out of ${total_fails} test suite fails."
       break
     fi
     ((limit--))
