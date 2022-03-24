0584b7e884984b341842f0f3c1c28a3f728b1db7
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-24 10:39:14 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -12,7 +12,7 @@ else
   BASE_DIR="${GITHUB_WORKSPACE}"
 fi
 
-REPORT_DIR="${BASE_DIR}/"
+REPORT_DIR="${BASE_DIR}"
 REPORT_FILE="index.json"
 
 # make sure report dir exists
@@ -22,10 +22,9 @@ touch "${REPORT_DIR}/${REPORT_FILE}"
 function generate_report(){
   local full_report=""
   local limit=3
-  local test_fail_counter=0
   local total_fails=`cat ${REPORT_FILE} | jq ".stats.failures"`
   local fail_results=`cat $REPORT_FILE | jq -r '[.results[].suites[].tests[] | select(.fail == true)']`
-  echo $fail_test_results
+  echo $fail_results
   local cypress_run_id=$(echo "${{ steps.run-integration.outputs.dashboardUrl }}" | sed 's:.*/::')
   for result in $(echo "${fail_results}" | jq -r '.[] | @base64'); do
     _jq() {
@@ -34,7 +33,6 @@ function generate_report(){
     title=$(_jq '.tests[0].title')
     file=$(_jq '.fullFile')
     message=$(_jq '.tests[0].err.message')
-    run_id=$(_jq '.uuid')
     report=$(echo ":test_tube:*TEST*: $title \n:open_file_folder:*FILE*: <https://cypress-dashboard.staging.manabie.io:31600/run/$cypress_run_id | $file> \n:speech_balloon:*MESSAGE*: $message \n\n")
     full_report+="$report"
     if [[ $limit -eq 0 ]]; then
@@ -42,11 +40,11 @@ function generate_report(){
     fi
     ((limit--))
     full_report=$(echo ${full_report//$'\n'/'%0A'} | sed 's/"//g')
-    echo "::set-output name=fail_count::$test_fail_counter"
+    echo "::set-output name=fail_count::$total_fails"
     echo "::set-output name=fail_report::$full_report"
   done
 }
 
 pushd "${REPORT_DIR}"
   generate_report
-popd
+popd 
