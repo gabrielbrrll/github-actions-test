a3ab3ff212dd12c63dc66401054742e22efe1193
Gab Ocampo <gabrielb.ocampo@gmail.com>
2022-03-28 12:37:00 +0800

    Update cypress-fail-report-transformer.bash

--- .github/workflows/scripts/cypress-fail-report-transformer.bash
+++ .github/workflows/scripts/cypress-fail-report-transformer.bash
@@ -36,7 +36,7 @@ function generate_report(){
     title=$(_jq '.title')
     parent_id=$(_jq '.parentUUID')
     file=`cat ${REPORT_FILE} | jq --arg parent_ref ${parent_id} '.results[].suites[] | select(.uuid == $parent_ref) | .fullFile'`
-    message=$(_jq '.err.message' | xargs)
+    message=$(_jq '.err.message')
     report=$(echo ":test_tube:*TEST*: $title \n:open_file_folder:*FILE*: <https://cypress-dashboard.staging.manabie.io:31600/run/$cypress_run_id | $file> \n:speech_balloon:*MESSAGE*: $message \n\n")
     full_report+="$report"
     
